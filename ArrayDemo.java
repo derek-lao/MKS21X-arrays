4ef8f31dd1639136336e80def4ff3dcd12572a8c
Derek Lao <dlao00@stuy.edu>
2018-09-28 01:14:42 -0400

    changed 'System.out.println(stringintarray2d(100))' to 'System.out.println(stringintarray2d(100,100))'

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -1,4 +1,5 @@
 public class ArrayDemo{
+//0a. Make a function to print a 1D array of ints.
 //1 dimensional array of integers
     public static String stringintarray1d (int n){
       String stringarray="{";
@@ -28,38 +29,45 @@ public class ArrayDemo{
     }
 //finish 1 dimensional array
 
+//0b. Make a function to print a 2d array of ints.
 //2 dimensional array of integers
-  public static String stringintarray2d (int n, int m){
-    String stringarray="{";
-    int[][] ary=new int[n][m];
-    for (int i=0;i<ary.length;i++)
-    {
-      for (int j=0;j<ary[i].length;j++)
+    public static String stringintarray2d (int n, int m){
+      String stringarray="{";
+      int[][] ary=new int[n][m];
+      for (int i=0;i<ary.length;i++)
       {
-        ary[i][j]=j+1;
-      }
-    }
-//made the array of integers
-    for (int p=0;p<ary.length;p++)
-    {
-      stringarray+= "\n{";
-      for (int q=0;q<ary[p].length;q++)
-      {
-        stringarray+= (ary[p][q]+"");
-        if(q<ary[p].length-1)
+        for (int j=0;j<ary[i].length;j++)
         {
-          stringarray+= ", ";
+          ary[i][j]=j+1;
         }
       }
-      stringarray+= "}";
+  //made the array of integers
+      for (int p=0;p<ary.length;p++)
+      {
+        stringarray+= "\n{";
+        for (int q=0;q<ary[p].length;q++)
+        {
+          stringarray+= (ary[p][q]+"");
+          if(q<ary[p].length-1)
+          {
+            stringarray+= ", ";
+          }
+        }
+        stringarray+= "}";
+      }
+      stringarray+= "\n}";
+      if (ary.length==0)
+      {
+        return "{}";
+      } //if array has length 0
+      return stringarray;
     }
-    stringarray+= "\n}";
-    if (ary.length==0)
-    {
-      return "{}";
-    } //if array has length 0
-    return stringarray;
-  }
+//finish 2 dimensional array
+
+//1. Calculate and return how many zeros are in the parameter
+
+
+
 
   public static void main(String[] args){
     System.out.println(stringintarray1d(0)) ;
@@ -69,14 +77,14 @@ public class ArrayDemo{
     System.out.println(stringintarray1d(4)) ;
     //System.out.println(stringintarray1d(100)) ;
     //tests for 1 dimensional array
-    
+
     System.out.println("\n");
     System.out.println(stringintarray2d(0,0)+"\n") ;
     System.out.println(stringintarray2d(1,1)+"\n") ;
     System.out.println(stringintarray2d(2,2)+"\n") ;
     System.out.println(stringintarray2d(3,3)+"\n") ;
     System.out.println(stringintarray2d(4,4)+"\n") ;
-    //System.out.println(stringintarray2d(100)) ;
+    //System.out.println(stringintarray2d(100,100)) ;
     //tests for 2 dimensional arrays
   }
 
