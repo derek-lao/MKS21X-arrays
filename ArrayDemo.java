fe303ca4e553df4cb3834ad382abb47f033bb7a1
derek-lao <43069580+derek-lao@users.noreply.github.com>
2018-09-29 20:23:41 -0400

    Update ArrayDemo.java
    updated manually from computer to github with keyboard.  may have inconsistencies.  will push from computer to github to check

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -40,93 +40,7 @@ public class ArrayDemo{
     System.out.println("}");
   }
 
-   public static String make2dstring(int n, int m){
-      int[][] ary=new int[n][m];
-      String stringarray="{";
-      for (int p=0;p<ary.length;p++)
-      {
-        stringarray+= "\n{";
-        for (int q=0;q<ary[p].length;q++)
-        {
-          stringarray+= (ary[p][q]+"");
-          if(q<ary[p].length-1)
-          {
-            stringarray+= ", ";
-          }
-        }
-        stringarray+= "}";
-      }
-      stringarray+= "\n}";
-      if (ary.length==0)
-      {
-        return "{}";
-      } //if array has length 0
-      return stringarray;
-    }
-    //code to turn 2d rectangular array into string
-    public static String stringintarray1d(int n){
-      String stringarray="{";
-      int[] ary=new int[n];
-      for (int i=0;i<ary.length;i++)
-      {
-        ary[i]=i+1;
-      }
-      //made the integer array
-      for (int i=0;i<ary.length;i++)
-      {
-        stringarray=stringarray+(ary[i]+"");
-        if (i<ary.length-1)
-        {
-          stringarray=stringarray+", ";
-        }
-        else
-        {
-          stringarray=stringarray+"}";
-        }
-      }
-      if (ary.length==0)
-      {
-        return "{}";
-      } //for exception of array having 0 length
-      return stringarray;
-    }
-//finish 1 dimensional array
-
-//0b. Make a function to print a 2d array of ints.
-//2 dimensional array of integers
-    public static String stringintarray2d(int n, int m){
-      int[][] ary=new int[n][m];
-      for (int i=0;i<ary.length;i++)
-      {
-        for (int j=0;j<ary[i].length;j++)
-        {
-          ary[i][j]=j+1;
-        }
-      }
-  //made the array of integers
-      String stringarray="{";
-      for (int p=0;p<ary.length;p++)
-      {
-        stringarray+= "\n{";
-        for (int q=0;q<ary[p].length;q++)
-        {
-          stringarray+= (ary[p][q]+"");
-          if(q<ary[p].length-1)
-          {
-            stringarray+= ", ";
-          }
-        }
-        stringarray+= "}";
-      }
-      stringarray+= "\n}";
-      if (ary.length==0)
-      {
-        return "{}";
-      } //if array has length 0
-      return stringarray;
-    }
-//finish 2 dimensional array
-
+   
 //1. Calculate and return how many zeros are in the parameter
 //(a rectangular 2D array of integers)
     public static int[][] int0array2d(int n, int m){
@@ -162,17 +76,6 @@ public class ArrayDemo{
 Replace all the of values with 1's
 EXCEPT when the row number is the same as the column number:
 you must fill those with 3's instead. */
-public static int[][] arrayofzeroes(int n, int m){
-  int[][] zeroarray= new int[n][m];
-  for (int i=0;i<zeroarray.length;i++)
-  {
-    for (int j=0;j<zeroarray[i].length;j++)
-    {
-      zeroarray[i][j]=0;
-    }
-  }
-  return zeroarray;
-}
 
 public static int[][] fill2D(int[][] ary){
   for (int i=0;i<ary.length;i++)
@@ -195,7 +98,29 @@ public static int[][] fill2D(int[][] ary){
   /* made array of all 1s, unless column number is equal to row number, then
   that is equal to 3 */
 
+/* 2b. Make a new 2d array the same dimensions as the given parameter.
+   Fill that array with with 1's,
+   except where the value of the original array is negative:
+   you must fill those with 3's instead. */
 
+
+   public static int[][] fill2Dcopy(int[][] ary){
+     for (int i=0;i<ary.length;i++)
+     {
+       for (int j=0;j<ary[i].length;j++)
+       {
+         if (i==j)
+         {
+           ary[i][j]=3;
+         }
+         else
+         {
+           ary[i][j]=1;
+         }
+       }
+     }
+     return ary;
+   }
 
   public static void main(String[] args){
     int[] ary1d={1,2,3};
@@ -225,10 +150,10 @@ public static int[][] fill2D(int[][] ary){
     };
     printarray(fill2D(samplearrayofanything));
     //expect every number per array to be 1, but every last number to be a 3
-    printarray(fill2D(arrayofzeroes(5,5))) ;
-    printarray(fill2D(arrayofzeroes(6,8))) ;
-    printarray(fill2D(arrayofzeroes(7,5))) ;
-    printarray(fill2D(arrayofzeroes(10,10))) ;
+    printarray(fill2D(int0array2d(5,5))) ;
+    printarray(fill2D(int0array2d(6,8))) ;
+    printarray(fill2D(int0array2d(7,5))) ;
+    printarray(fill2D(int0array2d(10,10))) ;
     //2 dimensional array of 1s, but if row#=column#, return 3
 
   }
